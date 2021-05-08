82b9b7b1e17ec4b4b38b7142ebf80b1d4ccbfee2
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-05-08 11:45:48 -0300

    Added SASS support

--- gatsby-bootcamp/src/components/Layout/Layout.js
+++ gatsby-bootcamp/src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React, { Children } from 'react';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
 
-
+import '../../styles/index.scss';
 
 export const Layout = (props) => {
   return (
