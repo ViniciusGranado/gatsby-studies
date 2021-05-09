465f870b12df1f5a2448e14df72c6a0b5d54edda
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-05-09 12:08:07 -0300

    Added SCSS modules

--- gatsby-bootcamp/src/components/Header/Header.js
+++ gatsby-bootcamp/src/components/Header/Header.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react"
+import { Link } from "gatsby"
+
+import * as headerStyles from "./Header.module.scss"
 
 export const Header = () => {
   return (
-    <header>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Vinicius Granado
+        </Link>
+      </h1>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/blog'>Blog</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link
+              to="/"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blog"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
-  );
-};
+  )
+}
--- gatsby-bootcamp/src/components/Layout/Layout.js
+++ gatsby-bootcamp/src/components/Layout/Layout.js
@@ -3,13 +3,17 @@ import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
 
 import '../../styles/index.scss';
+import * as layoutStyles from './Layout.module.scss';
 
 export const Layout = (props) => {
   return (
-    <>
-      <Header />
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
         {props.children}
+      </div>
+
       <Footer />
-    </>
+    </div>
   );
 };
